232e731c2659aa170b068581db24597f693ad2dd
grizzlyPi <lucasrodriguesdelima@rocketmail.com>
2020-02-08 20:15:09 -0300

    Exceptions

---
+++ src/application/Exceptions.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+import model.exceptions.DomainException;
+
+public class Exceptions {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			System.out.println("Enter account data");
+			System.out.print("Number: ");
+			Integer number = sc.nextInt();
+			sc.nextLine();
+			System.out.print("Holder: ");
+			String holder = sc.nextLine();
+			System.out.print("Initial balance: ");
+			Double balance = sc.nextDouble();
+			System.out.print("Withdraw limit: ");
+			Double withdrawLimit = sc.nextDouble();
+			System.out.println();
+			Account acc = new Account(number, holder, balance, withdrawLimit);
+			System.out.print("Enter amount for withdraw: ");
+			acc.withdraw(sc.nextDouble());
+			System.out.println("New balance: " + acc.getBalance());
+		}
+		catch(DomainException e) {
+			System.out.println("Withdraw error: " + e.getMessage());
+		}
+		sc.close();
+	}
+}
